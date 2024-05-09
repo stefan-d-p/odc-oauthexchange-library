c06a98a11ab3e493ee9c4724ccdcd1df9a183add
Stefan Weber <stefan.weber@spatium.one>
2024-05-09 07:22:55 +0200

    Fixed embedded images path

--- OAuthTokenExchange.Library/IOAuthTokenExchange.cs
+++ OAuthTokenExchange.Library/IOAuthTokenExchange.cs
@@ -5,14 +5,14 @@ namespace Without.Systems.OAuthTokenExchange
 {
     [OSInterface(
         Description = "Wrapper for operating with OAuth 2.0 compatible Authorization servers to exchange, refresh, introspect and revoke tokens. Includes utility action to generate Proof Key Code Exchange.",
-        IconResourceName = "Without.Systems.Resources.OAuth.png")]
+        IconResourceName = "Without.Systems.OAuthTokenExchange.Resources.OAuth.png")]
     public interface IOAuthTokenExchange
     {
         [OSAction(
             Description = "Request access token with client credentials flow",
             ReturnName = "result",
             ReturnDescription = "Access Token response structure",
-            IconResourceName = "Without.Systems.Resources.OAuthAction.png")]
+            IconResourceName = "Without.Systems.OAuthTokenExchange.Resources.OAuthAction.png")]
         TokenResult RequestClientCredentialsToken(
             [OSParameter(
                 Description = "Client credentials flow request")]
@@ -22,7 +22,7 @@ namespace Without.Systems.OAuthTokenExchange
             Description = "Exchange an authorization code for an access token",
             ReturnName = "result",
             ReturnDescription = "Access Token response structure",
-            IconResourceName = "Without.Systems.Resources.OAuthAction.png")]
+            IconResourceName = "Without.Systems.OAuthTokenExchange.Resources.OAuthAction.png")]
         TokenResult RequestAuthorizationCodeToken(
             [OSParameter(
                 Description = "Authorization Code exchange request")]
@@ -32,7 +32,7 @@ namespace Without.Systems.OAuthTokenExchange
             Description = "Refresh an access token with a refresh token",
             ReturnName = "result",
             ReturnDescription = "Access token response structure",
-            IconResourceName = "Without.Systems.Resources.OAuthAction.png")]
+            IconResourceName = "Without.Systems.OAuthTokenExchange.Resources.OAuthAction.png")]
         public TokenResult RequestRefreshToken(
             [OSParameter(
                 Description = "Refresh access token request")]
@@ -40,7 +40,7 @@ namespace Without.Systems.OAuthTokenExchange
 
         [OSAction(
             Description = "Revoke an access token",
-            IconResourceName = "Without.Systems.Resources.OAuthAction.png")]
+            IconResourceName = "Without.Systems.OAuthTokenExchange.Resources.OAuthAction.png")]
         public void RevokeToken(
             [OSParameter(
                 Description = "Revoke access token request")]
@@ -50,14 +50,14 @@ namespace Without.Systems.OAuthTokenExchange
             Description = "Generates code verifier and challange for Proof Key Code Exchange Authorization code flow",
             ReturnName = "result",
             ReturnDescription = "Proof Key Code details",
-            IconResourceName = "Without.Systems.Resources.OAuthAction.png")]
+            IconResourceName = "Without.Systems.OAuthTokenExchange.Resources.OAuthAction.png")]
         ProofKeyCode GenerateProofKeyCode();
 
         [OSAction(
             Description = "Introspect an access token",
             ReturnName = "isActive",
             ReturnDescription = "Indicates if the access token is active",
-            IconResourceName = "Without.Systems.Resources.OAuthAction.png")]
+            IconResourceName = "Without.Systems.OAuthTokenExchange.Resources.OAuthAction.png")]
         public bool IntrospectToken(
             [OSParameter(
                 Description = "Introspect access token request")]
@@ -67,7 +67,7 @@ namespace Without.Systems.OAuthTokenExchange
             Description = "Retrieve OpenID Connect Configuration of Identity Provider",
             ReturnName = "result",
             ReturnDescription = "OpenID Discovery Document",
-            IconResourceName = "Without.Systems.Resources.OAuthAction.png")]
+            IconResourceName = "Without.Systems.OAuthTokenExchange.Resources.OAuthAction.png")]
         public DiscoveryDocument GetDiscoveryDocument(
             [OSParameter(
                 Description = "Get Well-Known Configuration document request")]
